7dae2c83e5ee92184af48986606c0b213b6181de
JiArcangel0916 <arcangelalbrecht@gmail.com>
2024-10-26 23:40:16 +0800

    adjusted products section and responsiveness

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,11 +1,32 @@
-// // FUNCTION TO SCROLL INTO SECTION
-// function scrollIntoSection(sectionId) {
-//     const targetSec = document.querySelector(sectionId);
+document.addEventListener('DOMContentLoaded', function() {
+    let offers = document.querySelector('.offersFooter');
+    let offersSec = document.querySelector('#offers');
+    let message = document.querySelector('.messageFooter');
+    let messageSec = document.querySelector('#founderMsg')
+    let cant = document.querySelector('.cfFooter');
+    let cantSec = document.querySelector('#cantFindSection');
 
-//     if (targetSec) {
-//         targetSec.scrollIntoView({
-//             behavior: 'smooth',
-//             block: 'center'
-//         });
-//     }
-// }
+    offers.addEventListener('click', function(e){
+        e.preventDefault();
+        offersSec.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
+    });
+
+    message.addEventListener('click', function(e){
+        e.preventDefault();
+        messageSec.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
+    });
+
+    cant.addEventListener('click', function(e){
+        e.preventDefault();
+        cantSec.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
+    });
+});
